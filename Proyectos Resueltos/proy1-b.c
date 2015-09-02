b4e4296d16a9f4e695f0b56c5d1518c9ad1573e3
Administrator <crashbito@gmail.com>
2015-09-02 00:53:27 -0500

    Se agrega la solución al proyecto no.1
    Se agregan 2 soluciones al proyecto no.1, tratando al apuntador e iterando sobre el mismo, la otra solución es tratando al apuntador como si fuese un arreglo.

---
+++ Proyectos Resueltos/proy1-b.c
@@ -0,0 +1,55 @@
+// Proyecto no. 1, solucion B
+// Usar un apuntador y tratarlo como matriz de n * m , 
+// Indicar cuantos ceros tiene la matriz
+// Prohibido usar declaraciones de cualquier tipo de arreglos
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	int col, ren; //manejaran los tamannos de renglones y columnas de mi matriz
+	int *p ; // manejará la matriz
+	int i, j; // iteradores que se necesitaran en los "for"
+
+	int offset=0; // variable que me desplazara cada cierto numero de columnas
+
+	int total_ceros =0;  //variable que manejara el total de ceros
+
+	printf("Cuantos renglones?:");
+	scanf("%d", &ren);
+
+	printf("\nCuantas columnas?:");
+	scanf("%d", &col);
+
+	//Calculamos el tamanno de la matriz
+	
+	p = (int *)malloc((col * ren) * sizeof(int));
+
+	//Ahora pedimos nuestros datos 
+	for(i=0;i<ren;i++){
+		for(j=0; j<col; j++){
+			printf("\n\nIndique el valor para la posicion [%d][%d]", i, j);
+			scanf("%d", p + i + j + offset);
+			printf("posicion en el apuntador: %d", i+j+offset);  // podemos ver en que parte del apuntador vamos metiendo los datos
+		}
+		offset = offset + (col - 1); //Aqui avanzamos según el número de columnas :), este es el truco
+	}
+
+	printf("\n\n");
+	//presentamos la matriz y resetamos el offset
+	offset = 0;
+	for(i=0;i<ren;i++){
+		for(j=0;j<col;j++){
+			//Recorriendo solo el apuntador
+			if(*p == 0) total_ceros = total_ceros +1;
+
+			printf(" %d ", *p);
+			p++;
+		}
+		printf("\n");
+	}
+
+	printf("\nTotal de ceros: %d", total_ceros);
+
+}
